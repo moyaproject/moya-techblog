33f13b2eee69e8eabe3de23ae3ee31f2c85e4bfd
Will McGugan <willmcgugan@gmail.com>
2015-11-18 21:44:19 +0000

    heroku can't parse output

--- heroku/postdeploy.sh
+++ heroku/postdeploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-moya db sync
-moya auth#cmd.init --username $SUPER_USER --password $SUPER_PASSWORD --email $SUPER_EMAIL
-moya techblog#cmd.init --force
+moya db sync > /dev/null
+moya auth#cmd.init --username $SUPER_USER --password $SUPER_PASSWORD --email $SUPER_EMAIL > /dev/null
+moya techblog#cmd.init --force > /dev/null
