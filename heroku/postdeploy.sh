810117829ee6a6787fbf95a3ad6d7dda65e78618
Will McGugan <willmcgugan@gmail.com>
2015-11-19 23:16:43 +0000

    fixed for postdeploy

--- heroku/postdeploy.sh
+++ heroku/postdeploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-moya db sync > postdeploy.txt
-moya auth#cmd.init --username $SUPER_USER --password $SUPER_PASSWORD --email $SUPER_EMAIL > postdeploy.txt
-moya techblog#cmd.init --force > postdeploy.txt
+moya db sync
+moya auth#cmd.init --username $SUPER_USER --password $SUPER_PASSWORD --email $SUPER_EMAIL
+moya techblog#cmd.init --force
